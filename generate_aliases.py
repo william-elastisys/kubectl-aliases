d514115e04d92da185471d1bb426f2d916262397
William Johansson <william.johansson@elastisys.com>
2023-07-05 09:51:13 +0200

    added certificate (cert)

--- generate_aliases.py
+++ generate_aliases.py
@@ -79,6 +79,7 @@ def main():
         ('j',   'job',                               ['g', 'd', 'rm', 'e', 'c'], None),
         ('cj',  'cronjob',                           ['g', 'd', 'rm', 'e', 'c'], None),
         ('ep',  'endpoints',                         ['g', 'd', 'rm', 'e'],      None),
+        ('cert','certificates',                      ['g', 'd', 'rm', 'e'],      None),
         ('a',   'all',                               ['g'],                      None)
     ]
     res_types = [r[0] for r in res]
